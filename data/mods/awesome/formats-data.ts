436966f376b620e8ac5353b175eb338f62cc9022
GabrielMaleson <167476506+GabrielMaleson@users.noreply.github.com>
2024-08-23 23:46:47 -0300

    The "New"

---
+++ data/mods/awesome/formats-data.ts
@@ -0,0 +1,14 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	generalkillpain: {
+		tier: "Alternatium",
+		doublesTier: "Alternatium",
+	},
+	drpoopyfart: {
+		tier: "Alternatium",
+		doublesTier: "Alternatium",
+	},
+	poopotron8: {
+		tier: "Alternatium",
+		doublesTier: "Alternatium",
+	},
+};
