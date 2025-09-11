a37907cdc785f2915e613e6f2f4f55826e1c7563
GabrielMaleson <167476506+GabrielMaleson@users.noreply.github.com>
2025-09-11 15:50:44 -0300

    sprites

--- data/mods/auburn/formats-data.ts
+++ data/mods/auburn/formats-data.ts
@@ -22,13 +22,65 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	kingdedede: {
 		tier: "Auburn",
 		doublesTier: "Doubles Auburn",
-	},	
+	},
 	buzz: {
 		tier: "Auburn",
 		doublesTier: "Doubles Auburn",
-	},	
+	},
 	nightmarefredbear: {
 		tier: "Auburn",
 		doublesTier: "Doubles Auburn",
-	},	
+	},
+	supermachoman: {
+		tier: "Auburn",
+		doublesTier: "Doubles Auburn",
+	},
+	thejoker: {
+		tier: "Auburn",
+		doublesTier: "Doubles Auburn",
+	},
+	parapluey: {
+		tier: "Auburn",
+		doublesTier: "Doubles Auburn",
+	},
+	annabell: {
+		tier: "Auburn",
+		doublesTier: "Doubles Auburn",
+	},
+	bonzibuddy: {
+		tier: "Auburn",
+		doublesTier: "Doubles Auburn",
+	},
+	larbio: {
+		tier: "Auburn",
+		doublesTier: "Doubles Auburn",
+	},
+	raytelezza: {
+		tier: "Auburn",
+		doublesTier: "Doubles Auburn",
+	},
+	nostalgiacritic: {
+		tier: "Auburn",
+		doublesTier: "Doubles Auburn",
+	},
+	magolor: {
+		tier: "Auburn",
+		doublesTier: "Doubles Auburn",
+	},
+	ferry: {
+		tier: "Auburn",
+		doublesTier: "Doubles Auburn",
+	},
+	metaknight: {
+		tier: "Auburn",
+		doublesTier: "Doubles Auburn",
+	},
+	evilbrother: {
+		tier: "Auburn",
+		doublesTier: "Doubles Auburn",
+	},
+	vinnykong: {
+		tier: "Auburn",
+		doublesTier: "Doubles Auburn",
+	},
 };
